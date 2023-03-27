42b4703d555c50c5df606d44c679d7e944afef11
ruipedro84 <ruipasilveira@gmail.com>
2023-03-27 18:40:27 +0100

    login and guest functionality implemented

---
+++ src/data_structs/Graph.cpp
@@ -0,0 +1,157 @@
+// By: Gonçalo Leão
+
+#include "Graph.h"
+
+int Graph::getNumVertex() const {
+    return vertexSet.size();
+}
+
+std::vector<Vertex *> Graph::getVertexSet() const {
+    return vertexSet;
+}
+
+/*
+ * Auxiliary function to find a vertex with a given content.
+ */
+Vertex * Graph::findVertex(const int &id) const {
+    for (auto v : vertexSet)
+        if (v->getId() == id)
+            return v;
+    return nullptr;
+}
+
+
+Vertex* Graph::findVertex(const std::string name) const{
+    if(this->stList.find(name)==this->stList.end()) return nullptr;
+    return this->findVertex(this->stList.find(name)->second);
+}
+
+/*
+ * Finds the index of the vertex with a given content.
+ */
+int Graph::findVertexIdx(const int &id) const {
+    for (unsigned i = 0; i < vertexSet.size(); i++)
+        if (vertexSet[i]->getId() == id)
+            return i;
+    return -1;
+}
+/*
+ *  Adds a vertex with a given content or info (in) to a graph (this).
+ *  Returns true if successful, and false if a vertex with that content already exists.
+ */
+bool Graph::addVertex(Station s) {
+    int id = this->vertexSet.size();
+    vertexSet.push_back(new Vertex(id, s));
+    stList.insert({s.name,id});
+    return true;
+}
+
+/*
+ * Adds an edge to a graph (this), given the contents of the source and
+ * destination vertices and the edge weight (w).
+ * Returns true if successful, and false if the source or destination vertex does not exist.
+ */
+bool Graph::addEdge(const std::string &sourc, const std::string &dest, double w) {
+    auto v1 = findVertex(sourc);
+    auto v2 = findVertex(dest);
+    if (v1 == nullptr || v2 == nullptr)
+        return false;
+    v1->addEdge(v2, w);
+    return true;
+}
+
+bool Graph::addBidirectionalEdge(const std::string &sourc, const std::string &dest, double w) {
+    auto v1 = findVertex(sourc);
+    auto v2 = findVertex(dest);
+    if (v1 == nullptr || v2 == nullptr)
+        return false;
+    auto e1 = v1->addEdge(v2, w);
+    auto e2 = v2->addEdge(v1, w);
+    e1->setReverse(e2);
+    e2->setReverse(e1);
+    return true;
+}
+
+bool Graph::addBidirectionalEdge(Station s1, Station s2, double w) {
+    return this->addBidirectionalEdge(s1.name,s2.name,w);
+}
+
+void deleteMatrix(int **m, int n) {
+    if (m != nullptr) {
+        for (int i = 0; i < n; i++)
+            if (m[i] != nullptr)
+                delete [] m[i];
+        delete [] m;
+    }
+}
+
+void deleteMatrix(double **m, int n) {
+    if (m != nullptr) {
+        for (int i = 0; i < n; i++)
+            if (m[i] != nullptr)
+                delete [] m[i];
+        delete [] m;
+    }
+}
+
+Graph::~Graph() {
+    deleteMatrix(distMatrix, vertexSet.size());
+    deleteMatrix(pathMatrix, vertexSet.size());
+}
+
+void Graph::fordFulkerson(std::string src, std::string dest){
+    this->removeFlow();
+    Vertex *start = this->findVertex(src), *goal = this->findVertex(dest);
+    this->removePaths();
+    while(this->dfs(start,goal)) {
+        double flow = INF;
+        auto cur = goal;
+        while(cur!=start){
+            flow = std::min(flow,(cur->getPath()->getWeight())-abs(cur->getPath()->getFlow()));
+            cur = cur->getPath()->getDest();
+        }
+
+        cur = goal;
+        while(cur!=start){
+            cur->getPath()->setFlow(cur->getPath()->getFlow()-flow);
+            cur->getPath()->getReverse()->setFlow(cur->getPath()->getReverse()->getFlow()+flow);
+            cur = cur->getPath()->getDest();
+        }
+
+
+        this->removePaths();
+    }
+
+}
+
+bool Graph::dfs(std::string src, std::string dest){
+    Vertex *start = this->findVertex(src), *goal = this->findVertex(dest);
+    return this->dfs(start,goal);
+}
+
+bool Graph::dfs(Vertex* src, Vertex* dest) {
+    if(src==dest) return true;
+    for(auto &i : src->getAdj()){
+        if(i->getWeight()>abs(i->getFlow()) && i->getDest()->getPath() == nullptr)
+        {
+            i->getDest()->setPath(i->getReverse());
+            if(this->dfs(i->getDest(),dest)) return true;
+        }
+    }
+    return false;
+}
+
+void Graph::removePaths() {
+    for(auto &i : this->vertexSet) i->setPath(nullptr);
+}
+
+void Graph::removeFlow(){
+    for(auto &i : this->vertexSet) for(auto &j : i->getAdj()) j->setFlow(0.0);
+}
+
+double Graph::maxInPath(std::string src, std::string dest){
+    this->fordFulkerson(src,dest);
+    double out = 0.0;
+    for(auto &i : this->findVertex(dest)->getIncoming()) if(i->getFlow()>0) out+=i->getFlow();
+    return out;
+}
---
+++ src/data_structs/Graph.h
@@ -0,0 +1,71 @@
+// By: Gonçalo Leão
+
+#ifndef DA_TP_CLASSES_GRAPH
+#define DA_TP_CLASSES_GRAPH
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <algorithm>
+#include <unordered_map>
+
+#include "VertexEdge.h"
+
+class Graph {
+public:
+    ~Graph();
+    /*
+    * Auxiliary function to find a vertex with a given ID.
+    */
+    Vertex *findVertex(const int &id) const;
+    Vertex *findVertex(const std::string name) const;
+    /*
+     *  Adds a vertex with a given content or info (in) to a graph (this).
+     *  Returns true if successful, and false if a vertex with that content already exists.
+     */
+    bool addVertex(Station s);
+
+    /*
+     * Adds an edge to a graph (this), given the contents of the source and
+     * destination vertices and the edge weight (w).
+     * Returns true if successful, and false if the source or destination vertex does not exist.
+     */
+    bool addEdge(const std::string &sourc, const std::string &dest, double w); //A principio n se vai usar
+    bool addBidirectionalEdge(const std::string &sourc, const std::string &dest, double w);
+
+    bool addBidirectionalEdge(Station s1, Station s2, double w); //Funcao pq sou preguiçoso
+
+    int getNumVertex() const;
+    std::vector<Vertex *> getVertexSet() const;
+
+
+
+    void fordFulkerson(std::string src, std::string dest); //A funcao q importa
+    bool dfs(Vertex* src, Vertex* dest);
+    bool dfs(std::string src, std::string dest);
+
+    void removePaths();
+    void removeFlow();
+
+    double maxInPath(std::string src, std::string dest); //4.1 T2.1 3.5 valores <3
+
+    std::unordered_map<std::string, int> stList;
+protected:
+    std::vector<Vertex *> vertexSet;    // vertex set
+
+    double ** distMatrix = nullptr;   // dist matrix for Floyd-Warshall
+    int **pathMatrix = nullptr;   // path matrix for Floyd-Warshall
+
+    /*
+     * Finds the index of the vertex with a given content.
+     */
+    int findVertexIdx(const int &id) const;
+
+
+};
+
+void deleteMatrix(int **m, int n);
+void deleteMatrix(double **m, int n);
+
+#endif /* DA_TP_CLASSES_GRAPH */
